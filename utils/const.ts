9fe98ea420d9bbf7471e68ee785c382bf070db47
Rahul Aher <aherrahul1995@gmail.com>
2024-05-11 21:35:05 +0530

    first commit

---
+++ utils/const.ts
@@ -0,0 +1,12 @@
+export const SOCIALS = {
+  twitter: 'https://twitter.com/TheAlexLichter/',
+  mastodon: 'https://hachyderm.io/@manniL',
+  github: 'https://github.com/aherrahul/',
+  youtube: 'https://www.youtube.com/@TheAlexLichter/',
+  twitch: 'https://www.twitch.tv/TheAlexLichter/',
+  linkedin: 'https://www.linkedin.com/in/alexanderlichter/',
+} as const
+
+export const LOCALSTORAGE_KEYS = {
+  notificationGerman: 'notification-german',
+} as const
